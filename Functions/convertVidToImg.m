5c9e4d8ea708f4ebb6f467bf1955978e4f42b846
ARamwell <rmwaly001@myuct.ac.za>
2024-10-18 10:38:56 +0200

    Added .gitignore and changed file references in convertVidToImage to be relative.

--- Functions/convertVidToImg.m
+++ Functions/convertVidToImg.m
@@ -1,5 +1,5 @@
 %Script to import video from file, convert each frame to an image, and save
 %those images to the same folder
-vidFile = fullfile("C:\Users\alyss\OneDrive - University of Cape Town\Sandbox\PnP\Pnp_solver\GitClone\SimEnv\Results\camOutput.avi");
+vidFile = fullfile('.', '/QuadSimEnv/Results/Traj-0005/camOutput.avi');
 targetFolder = fileparts(vidFile);
 imgFuncs.convertVideo(vidFile,targetFolder);
